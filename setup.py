fdf949f77ec9ff93b36d7b13193a732357e4fe56
Jonathan Tay <jonathantay193@gmail.com>
2021-05-13 16:56:24 +0100

    boilerplate code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting housing prices on Kaggle.',
+    author='Jonathan Tay',
+    license='MIT',
+)
