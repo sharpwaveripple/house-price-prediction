854d12ee37d6ddd5c012ee1df067ff3cbb6c9922
Jonathan Tay <jonathantay193@gmail.com>
2021-05-14 16:31:44 +0100

    wrote main function

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,6 +3,9 @@ import os
 
 import pandas as pd
 
+import get_important_features
+from utils import encode_categoricals, split_data
+
 
 def read_data(project_dir):
     fpath = os.path.join(project_dir, "data", "interim", "df.csv")
@@ -10,12 +13,12 @@ def read_data(project_dir):
     return pd.read_csv(fpath)
 
 
-def calc_datesold(df, drop_orig_col=True):
-    print("Creating DateSold from YrSold + MoSold * .01")
-    df["DateSold"] = df["YrSold"] + df["MoSold"] * 0.01
-    if drop_orig_col:
-        print("Dropping YrSold and MoSold")
-        df.drop(columns=["YrSold", "MoSold"], inplace=True)
+# def calc_datesold(df, drop_orig_col=True):
+#     print("Creating DateSold from YrSold + MoSold * .01")
+#     df["DateSold"] = df["YrSold"] + df["MoSold"] * 0.01
+#     if drop_orig_col:
+#         print("Dropping YrSold and MoSold")
+#         df.drop(columns=["YrSold", "MoSold"], inplace=True)
 
 
 # def drop_collinear(df):
@@ -23,7 +26,7 @@ def calc_datesold(df, drop_orig_col=True):
 #     df.drop(columns=drop_list, inplace=True)
 
 
-def write_categorical(df, write=True):
+def write_categoricals(df, write=True):
     categoricals = []
     for col in df.columns:
         if df[col].dtype == "object" or col == "MSSubClass":
@@ -39,7 +42,32 @@ def write_categorical(df, write=True):
         open(fpath, "w+").writelines("\n".join(categoricals))
 
 
-project_dir = "../../"
+def filter_variables(df, project_dir):
+    write_categoricals(df)
+    fpath = os.path.join(project_dir, "src", "features", "X.txt")
+    if os.path.exists(fpath):
+        print(f"{fpath} found, loading")
+    else:
+        print(f"{fpath} not found, using feature importance pipeline")
+        get_important_features.main()
 
-df = read_data(project_dir)
-write_categorical(df)
+    X_list = open(fpath, "r").read().splitlines()
+    X_list.extend(["Id", "SalePrice", "dataset"])
+    return X_list
+
+
+def main():
+    project_dir = "../../"
+    df = read_data(project_dir)
+
+    X_list = filter_variables(df, project_dir)
+    X_list.remove("GarageYrBlt")
+
+    # one hot encode and write out
+    df_cat = encode_categoricals(df[X_list], project_dir, "one_hot")
+
+    train, test = split_data(df_cat)
+    for i, j in {'train': train, 'test': test}.items():
+        fpath = os.path.join(project_dir, "data", "processed", f"{i}.csv")
+        print(f"Saving processed {i}ing data to {fpath}")
+        j.to_csv(fpath, index=None)
