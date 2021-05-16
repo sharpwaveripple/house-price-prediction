8015859af56d4c8bb500335401b03b98b600f690
Jonathan Tay <jonathantay193@gmail.com>
2021-05-16 20:29:13 +0100

    main method

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,75 +1,70 @@
+import numpy as np
 import pandas as pd
 from lightgbm import LGBMRegressor
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score, train_test_split
 from skopt import BayesSearchCV
 from skopt.space import Categorical, Integer, Real
+import pickle
+import os
 
-
-def optimize(
-    X_train, y_train, X_test, y_test, estimator, search_spaces, n_iter, cv, random_state
+# https://stackoverflow.com/questions/42228735/scikit-learn-gridsearchcv-with-multiple-repetitions/42230764#42230764
+def nested_cv(
+    estimator, search_spaces, X, y,
+    scoring="neg_mean_squared_error",
+    inner_cv=5, outer_cv=10, random_state=42,
 ):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.1, random_state=random_state,
+    )
+
     opt = BayesSearchCV(
-        estimator=estimator,
-        search_spaces=search_spaces,
-        n_iter=n_iter,
-        cv=cv,
-        verbose=0,
-        n_jobs=4,
-        random_state=1,
+        estimator=estimator, search_spaces=search_spaces,
+        scoring="neg_mean_squared_error", n_iter=25, cv=inner_cv,
+        verbose=0, n_jobs=4, random_state=random_state,
     )
     opt.fit(X_train, y_train)
+    print("Best params:\n%s" % opt.best_params_)
+    inner_cv_rmse = np.round(np.sqrt(opt.best_score_ * -1), 2)
+    print(f"Inner loop RMSE: {inner_cv_rmse}")
 
-    train_score = opt.best_score_ * 100
-    test_score = opt.score(X_test, y_test) * 100
+    nested_score = cross_val_score(
+        opt, X_train, y_train, cv=outer_cv, scoring=scoring, n_jobs=4,
+    )
+    outer_cv_rmse = np.round(np.sqrt(nested_score.mean() * -1), 2)
+    print(f"Outer loop RMSE: {outer_cv_rmse}")
 
-    print("Best estimator:\n%s" % opt.best_estimator_)
-    print("Validation score: %.1f%%" % train_score)
-    print("Test score: %.1f%%" % test_score)
-
+    y_pred = opt.predict(X_test)
+    rmse = np.round(mean_squared_error(y_test, y_pred, squared=False), 2)
+    print(f"Validation RMSE: {rmse}")
     return opt
 
 
-# load train and test data
-df = pd.read_csv("../../data/processed/train.csv")
+def main():
+    # load train and test data
+    project_dir = "../../"
+    fpath = os.path.join(project_dir, "data", "processed", "train.csv")
+
+    df = pd.read_csv(fpath)
+
+    y = df["SalePrice"]
+    X = df[[x for x in df.columns if x not in ["Id", "SalePrice"]]]
+
+    random_state = 0
+
+    estimator = LGBMRegressor(
+        boosting_type="gbdt", random_state=random_state
+    )
+    search_spaces = {
+        "n_estimators": Integer(100, 1000),
+        "learning_rate": Real(1e-4, 1e-1),
+    }
+    opt = nested_cv(estimator, search_spaces, X, y, random_state=random_state)
+
+    model_out = os.path.join(project_dir, "models", "LGBM.pkl")
+    print(f"Saving model for  to {model_out}")
+    pickle.dump(opt, open(model_out, "wb"))
 
 
-X = df[[x for x in df.columns if x != "SalePrice"]]
-y = df["SalePrice"]
-
-random_state = 3
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.4,
-    random_state=random_state,
-)
-
-search_spaces = {
-    "learning_rate": Real(0.01, 0.1, "log-uniform"),
-    "n_estimators": Integer(100, 1000),
-}
-
-estimator = LGBMRegressor(
-    objective="regression_l1", random_state=random_state,
-)
-
-opt = BayesSearchCV(
-    estimator=estimator,
-    search_spaces=search_spaces,
-    n_iter=100,
-    cv=25,
-    verbose=0,
-    n_jobs=4,
-    random_state=random_state,
-)
-opt.fit(X_train, y_train)
-
-train_score = opt.best_score_ * 100
-test_score = opt.score(X_test, y_test) * 100
-
-print("Best estimator:\n%s" % opt.best_estimator_)
-print("Validation score: %.1f%%" % train_score)
-print("Test score: %.1f%%" % test_score)
-
-score = cross_val_score(opt, X_train, y_train, cv=10)
-print(score)
+if __name__ == "__main__":
+    main()
